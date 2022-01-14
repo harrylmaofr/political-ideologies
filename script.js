3a7a4df75385a8f584d767b8b32e5b63dd0fd9bd
Harry Rizwan <97665189+harrylmaofr@users.noreply.github.com>
2022-01-14 18:49:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("welcome to my cspe cba 😸 - haroon");
+
+function changetextx() {
+  document.getElementById("var")
+    .innerHTML = "m e"
+}
+
+function changebackx() {
+  document.getElementById("var")
+    .innerHTML = "me"
+}
+
+function changetext1() {
+  document.getElementById("var2")
+    .innerHTML = "d o c u m e n t"
+}
+
+function changeback2() {
+  document.getElementById("var2")
+    .innerHTML = "document"
+}
